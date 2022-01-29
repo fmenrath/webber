4e3942c680ce8393d794e569661cc147e2771fd5
Fabian Menrath <menrath.fabian@gmail.com>
2022-01-29 18:31:11 +0100

    Changed about page text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="about-wrapper">
       <h1>About</h1>
-      <p>Webber is a database for movies and TV shows was developed as a personal project in January 2022. <br>
+      <p>Webber is a website to browse and bookmark movies and TV shows. It was developed as a personal project in January 2022. <br>
          The goal of this project was to learn the basics of VueJS 3 and SCSS, while also improving on my design skills.<br><br>
          The data used for the app is being retrieved with AJAX calls from "The Movie Database API v3".</p>
       <img src="../assets/movieDBlogo.png" alt="" id="movie-db-logo">
